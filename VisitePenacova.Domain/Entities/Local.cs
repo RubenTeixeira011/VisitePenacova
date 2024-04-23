a9d7743df7ddea430ac6c5c4bfc17f75b2cb50ea
RubenTeixeira011 <rubenteixeira011@gmail.com>
2024-04-23 22:41:33 +0100

    Sessão 1

---
+++ VisitePenacova.Domain/Entities/Local.cs
@@ -0,0 +1,15 @@
+﻿namespace VisitePenacova.Domain.Entities
+{
+    public class Local
+    {
+        public int Id { get; set; }
+        public required string Nome { get; set; }
+        public string? Descricao { get; set; }
+        public required string Localidade { get; set; }
+        public required string Freguesia { get; set; }
+        public required string Categoria { get; set; }
+        public string? ImageUrl { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+    }
+}
